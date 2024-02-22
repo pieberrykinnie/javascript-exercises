7565bc8d1a7a2482792c351fdf5dfc5d9d16ea9b
Peter Vu <dmtruong1980@gmail.com>
2024-02-22 15:18:13 -0600

    feat: Complete exercise 8
    Didn't know Array.reduce() existed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+
+  for (num of arr) {
+    sum += num;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 0;
 
+  if (arr.length > 0) {
+    product = 1;
+
+    for (num of arr) {
+      product *= num;
+    }
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
