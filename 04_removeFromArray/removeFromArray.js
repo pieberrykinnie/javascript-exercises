ebe6e6131d12796f069b208f00d7d2e3fd7b0015
Peter Vu <dmtruong1980@gmail.com>
2024-02-22 11:36:45 -0600

    feat: Complete exercise 4: Remove from array
    Use the filter method to removes a dynamic amount of elements from an
    array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (num of nums) {
+        arr = arr.filter((ele) => ele !== num);
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
