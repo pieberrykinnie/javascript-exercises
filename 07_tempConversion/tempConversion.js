7ea9474a4734f93da4e42a97b681b512820d6e74
Peter Vu <dmtruong1980@gmail.com>
2024-02-22 15:10:44 -0600

    feat: Complete exercise 7: Temperature conversion
    Convert temperatures and round them to 1 digit, if necessary. The
    solution was much smarter than mine, but oh well.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+
+  return Number.isInteger(tempC) ? tempC : +(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC * 9 / 5 + 32;
+
+  return Number.isInteger(tempF) ? tempF : +(tempF.toFixed(1));
 };
 
 // Do not edit below this line
