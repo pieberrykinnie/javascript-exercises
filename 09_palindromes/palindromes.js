d6a776f0f9a33967c383f0345c35900389c3e74e
Peter Vu <dmtruong1980@gmail.com>
2024-03-06 01:26:40 -0600

    feat: Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const charList = str.split("")
+    .filter(e => e.toUpperCase() !== e.toLowerCase() || Number(e) > 0)
+    .map(e => e.toLowerCase());
 
+    const reversedCharList = charList.slice().reverse();
+
+    return charList.every((e, i) => e === reversedCharList[i]);
 };
 
 // Do not edit below this line
