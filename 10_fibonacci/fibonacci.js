7b0c3a4d9a76437dd117f6ef5d898d66d5f86d35
Peter Vu <dmtruong1980@gmail.com>
2024-03-06 01:39:01 -0600

    feat: Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0 || !Number.isInteger(Number(num))) {
+        return "OOPS";
+    } else if (Number(num) === 0) {
+        return 0;
+    } else if (Number(num) < 3) {
+        return 1;
+    }else {
+        const fibList = [1, 1];
 
+        for (let i = 0; i < Number(num) - 2; i++) {
+            fibList[i + 2] = fibList[i] + fibList[i + 1];
+        }
+
+        return fibList[Number(num) - 1];
+    }
 };
 
 // Do not edit below this line
