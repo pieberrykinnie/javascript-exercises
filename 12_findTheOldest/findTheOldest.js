32451b3e2eb5d57a16d9b7de43e50f1e17803f39
Peter Vu <dmtruong1980@gmail.com>
2024-03-06 02:01:21 -0600

    feat: Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.map((e) => ({
+            name: e.name,
+            age: e.yearOfDeath ? e.yearOfDeath - e.yearOfBirth : new Date().getFullYear() - e.yearOfBirth,
+        })).reduce((target, curr) => curr.age > target.age ? curr : target);
 };
 
 // Do not edit below this line
